6b733d2bdb188a4424c15d2fc7b149780c1b77f7
wangyinhe <wyinhe@sina.cn>
2026-05-17 22:03:01 +0800

    验证回文串 II

--- src/string/palindrome/valid_palindrome.rs
+++ src/string/palindrome/valid_palindrome.rs
@@ -3,6 +3,35 @@ struct Solution;
 
 impl Solution {
     pub fn valid_palindrome(s: String) -> bool {
+        if s.is_empty() {
+            return true;
+        }
+        let mut i = 0;
+        let mut j = s.len() - 1;
+        let s = s.as_bytes();
+        while i < j {
+            if s[i] != s[j] {
+                return Self::is_palindrome(&s[i + 1..j + 1]) || Self::is_palindrome(&s[i..j]);
+            }
+            i += 1;
+            j -= 1;
+        }
+        true
+    }
+
+    fn is_palindrome(s: &[u8]) -> bool {
+        if s.is_empty() {
+            return true;
+        }
+        let mut i = 0;
+        let mut j = s.len() - 1;
+        while i < j {
+            if s[i] != s[j] {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
         true
     }
 }
