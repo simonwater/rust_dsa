224f7d4e03a12af9d563dd22a997c4d7a3b301e5
wangyinhe <wyinhe@sina.cn>
2026-05-28 12:06:31 +0800

    栈：逆波兰表达式求值

---
+++ src/stack/eval_rpn.rs
@@ -0,0 +1,40 @@
+/// [150. 逆波兰表达式求值](https://leetcode.cn/problems/evaluate-reverse-polish-notation/)
+struct Solution;
+
+/// 栈
+/// 注意栈容量的计算
+impl Solution {
+    pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+        // 极端情况遇到数字都在左边，符号都在右边的表达式，这时最大长度就是：tokens.len() / 2 + 1
+        let mut stack = Vec::with_capacity(tokens.len() / 2 + 1);
+        for token in tokens {
+            match token.as_str() {
+                "+" | "-" | "*" | "/" => {
+                    let b = stack.pop().unwrap();
+                    let a = stack.pop().unwrap();
+                    let val = match token.as_str() {
+                        "+" => a + b,
+                        "-" => a - b,
+                        "*" => a * b,
+                        "/" => a / b,
+                        _ => unreachable!(),
+                    };
+                    stack.push(val);
+                }
+                _ => {
+                    let val: i32 = token.parse().unwrap();
+                    stack.push(val);
+                }
+            }
+        }
+        stack.pop().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -1,3 +1,4 @@
 pub mod backspace_compare;
+pub mod eval_rpn;
 pub mod my_queue;
 pub mod valid_parentheses;
