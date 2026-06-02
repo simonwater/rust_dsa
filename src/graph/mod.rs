66b438d1da0498680f528c6ffe38111e5bc044a7
wangyinhe <wyinhe@sina.cn>
2026-06-02 15:41:33 +0800

    图：冗余连接。并查集路径压缩

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -4,6 +4,7 @@ pub mod max_area_of_island;
 pub mod node;
 pub mod num_islands;
 pub mod number_of_provinces;
+pub mod redundant_connection;
 pub mod rotting_oranges;
 pub mod surrounded_regions;
 pub mod update_matrix;
---
+++ src/graph/redundant_connection.rs
@@ -0,0 +1,35 @@
+/// [684. 冗余连接](https://leetcode.cn/problems/redundant-connection/)
+struct Solution;
+
+impl Solution {
+    pub fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
+        let n = edges.len();
+        let mut parents: Vec<usize> = (0..=n).collect();
+        for edge in edges {
+            let (u, v) = (edge[0] as usize, edge[1] as usize);
+            let u_id = Self::find(u, &mut parents);
+            let v_id = Self::find(v, &mut parents);
+            if u_id == v_id {
+                return edge;
+            }
+            parents[u_id] = v_id;
+        }
+        vec![]
+    }
+
+    fn find(mut val: usize, parents: &mut [usize]) -> usize {
+        if parents[val] == val {
+            return val;
+        }
+        parents[val] = Self::find(parents[val], parents);
+        parents[val]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
