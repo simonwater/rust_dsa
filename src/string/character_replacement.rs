7f0db78e6fc80f168e06dce359da9bbb747973eb
wangyinhe <wyinhe@sina.cn>
2026-06-05 12:57:52 +0800

    字符串：替换后的最长重复字符，滑动窗口

---
+++ src/string/character_replacement.rs
@@ -0,0 +1,47 @@
+/// [424. 替换后的最长重复字符](https://leetcode.cn/problems/longest-repeating-character-replacement/)
+struct Solution;
+
+impl Solution {
+    pub fn character_replacement(s: String, k: i32) -> i32 {
+        let n = s.len() as i32;
+        if k >= n - 1 {
+            return n as i32;
+        }
+        let s_bytes = s.as_bytes();
+        let mut cnt = [0i32; 26];
+        let mut ans = k + 1;
+        let mut left = 0usize;
+        let k_usize = k as usize;
+        for (right, &c) in s_bytes.iter().enumerate() {
+            cnt[(c - b'A') as usize] += 1;
+            if right <= k_usize {
+                continue;
+            }
+
+            if !Self::check(&cnt, k, (right - left) as i32 + 1) {
+                // 左边界只需收缩一位，没必要循环得到一个可行解，因为当前右边界已经固定，只收缩左边界得到的解肯定不可能更好。
+                cnt[(s_bytes[left] - b'A') as usize] -= 1;
+                left += 1;
+            }
+            ans = ans.max((right - left) as i32 + 1);
+        }
+
+        ans
+    }
+
+    fn check(cnt: &[i32; 26], k: i32, total: i32) -> bool {
+        if k >= total - 1 {
+            return true;
+        }
+        let max = cnt.iter().max().unwrap();
+        return k >= total - max;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
--- src/string/mod.rs
+++ src/string/mod.rs
@@ -1,3 +1,4 @@
+pub mod character_replacement;
 pub mod find_anagrams;
 pub mod group_anagrams;
 pub mod length_of_longest_substring;
