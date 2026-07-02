1c92500e7821559767a0fe2c318d4c0b45d90bb6
wangyinhe <wyinhe@sina.cn>
2026-07-02 10:47:24 +0800

    堆：数据流的中位数

---
+++ src/heap/median_finder.rs
@@ -0,0 +1,47 @@
+/// [295. 数据流的中位数](https://leetcode.cn/problems/find-median-from-data-stream/)
+struct MedianFinder {
+    left_max: BinaryHeap<i32>,
+    right_min: BinaryHeap<Reverse<i32>>,
+}
+
+use std::{cmp::Reverse, collections::BinaryHeap};
+impl MedianFinder {
+    fn new() -> Self {
+        Self {
+            left_max: BinaryHeap::with_capacity(128),
+            right_min: BinaryHeap::with_capacity(128),
+        }
+    }
+
+    fn add_num(&mut self, num: i32) {
+        if self.left_max.len() == self.right_min.len() {
+            match self.right_min.peek() {
+                Some(&Reverse(val)) if num > val => {
+                    let Reverse(val) = self.right_min.pop().unwrap();
+                    self.left_max.push(val);
+                    self.right_min.push(Reverse(num));
+                }
+                _ => self.left_max.push(num),
+            }
+        } else {
+            match self.left_max.peek() {
+                Some(&val) if num < val => {
+                    self.left_max.pop();
+                    self.right_min.push(Reverse(val));
+                    self.left_max.push(num);
+                }
+                _ => self.right_min.push(Reverse(num)),
+            }
+        }
+    }
+
+    fn find_median(&self) -> f64 {
+        if self.left_max.len() > self.right_min.len() {
+            return *self.left_max.peek().unwrap() as f64;
+        } else {
+            let left_val = *self.left_max.peek().unwrap();
+            let Reverse(right_val) = self.right_min.peek().unwrap();
+            return (left_val + right_val) as f64 / 2.0;
+        }
+    }
+}
--- src/heap/mod.rs
+++ src/heap/mod.rs
@@ -1,4 +1,5 @@
 pub mod k_closest;
 pub mod k_closest_elements;
 pub mod kth_largest;
+pub mod median_finder;
 pub mod top_k_frequent_word;
